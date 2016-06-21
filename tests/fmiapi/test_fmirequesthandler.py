a7b6f0d39934851f5427d0e70999f52ecc35548d
Tuomas Salmi <salmi.tuomas@gmail.com>
2016-06-21 23:40:28 +0300

    Tests written to use mocking for request handler.

--- tests/fmiapi/test_fmirequesthandler.py
+++ tests/fmiapi/test_fmirequesthandler.py
@@ -1,40 +1,73 @@
 from fmiapi.fmirequesthandler import FMIRequestHandler
 from datetime import datetime
 from tests.testUtils import *
+from unittest import mock
+from unittest.mock import call
+import pytz
 
-
+timezone = pytz.timezone('Europe/Helsinki')
 
 
 class TestFMIRequestHandler():
-
     def setup(self):
         self.fmi_handler = FMIRequestHandler('apikey')
         self._DAILY_REQUEST_MAX_RANGE_HOURS = 8928
         self._REALTIME_REQUEST_MAX_RANGE_HOURS = 168
 
-    def should_get_year_in_one_request(self):
-        start_time = datetime.strptime('2010-01-01T00:01', '%Y-%m-%dT%H:%M')
-        end_time = datetime.strptime('2010-01-05T00:01', '%Y-%m-%dT%H:%M')
-        expected_count = 1
-        count = self.fmi_handler._required_requests_count(start_time, end_time, self._DAILY_REQUEST_MAX_RANGE_HOURS)
-        assertEqual(expected_count, count)
+    @mock.patch('fmiapi.fmirequesthandler.FMIRequest', spec=True)
+    def should_get_year_in_one_request(self, mock_fmirequest):
+        query = {'request': 'getFeature',
+                 'storedquery_id': 'fmi::observations::weather::daily::multipointcoverage',
+                 'fmisid': '1234',
+                 'starttime': datetime(2010, 1, 1, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                 'endtime': datetime(2011, 1, 5, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone)
+                 }
 
-    def should_count_2_request_for_over_372_day_time_span(self):
-        start_time = datetime.strptime('2010-01-01T00:01', '%Y-%m-%dT%H:%M')
-        end_time = datetime.strptime('2011-01-09T00:01', '%Y-%m-%dT%H:%M')
-        expected_count = 2
-        count = self.fmi_handler._required_requests_count(start_time, end_time, self._DAILY_REQUEST_MAX_RANGE_HOURS)
-        assertEqual(expected_count, count)
+        expected = {'starttime': datetime(2010, 1, 1, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                    'endtime': datetime(2011, 1, 5, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                    'fmisid': '1234',
+                    'request': 'getFeature',
+                    'storedquery_id': 'fmi::observations::weather::daily::multipointcoverage'
+                    }
 
-    def should_prepare_2_requests_for_over_372_day_time_span(self):
-        start_time = datetime.strptime('2010-01-01T00:01', '%Y-%m-%dT%H:%M')
-        end_time = datetime.strptime('2011-01-23T00:01', '%Y-%m-%dT%H:%M')
-        expected_count = 2
-        requests = self.fmi_handler._divide_to_multiple_requests({"starttime": start_time,
-                                                                  "endtime": end_time},
-                                                                 self._DAILY_REQUEST_MAX_RANGE_HOURS)
-        assertEqual(expected_count, len(requests))
-        assertEqual('2010-01-01', requests[0]["starttime"].strftime('%Y-%m-%d'))
-        assertEqual('2011-01-07', requests[0]["endtime"].strftime('%Y-%m-%d'))
-        assertEqual('2011-01-08', requests[1]["starttime"].strftime('%Y-%m-%d'))
-        assertEqual('2011-01-23', requests[1]["endtime"].strftime('%Y-%m-%d'))
+        mock_instance = mock_fmirequest.return_value
+        mock_instance.get.return_value = 'data'
+
+        handler = FMIRequestHandler('apikey')
+        result = handler.request(query, max_timespan=self._DAILY_REQUEST_MAX_RANGE_HOURS, progress_callback=None)
+
+        mock_instance.get.assert_has_calls([call(expected)])
+        assertEqual(1, mock_instance.get.call_count)
+        assertEqual(1, len(result))
+
+    @mock.patch('fmiapi.fmirequesthandler.FMIRequest', spec=True)
+    def should_call_fmirequest_in_two_parts_for_372_day_time_span(self, mock_fmirequest):
+        query = {'request': 'getFeature',
+                 'storedquery_id': 'fmi::observations::weather::daily::multipointcoverage',
+                 'fmisid': '1234',
+                 'starttime': datetime(2010, 1, 1, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                 'endtime': datetime(2011, 1, 23, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone)
+                 }
+        expected = [{'starttime': datetime(2010, 1, 1, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                     'endtime': datetime(2011, 1, 7, hour=23, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                     'fmisid': '1234',
+                     'request': 'getFeature',
+                     'storedquery_id': 'fmi::observations::weather::daily::multipointcoverage'
+                     },
+                    {'starttime': datetime(2011, 1, 8, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                     'endtime': datetime(2011, 1, 23, hour=0, minute=1, second=0, microsecond=0, tzinfo=timezone),
+                     'fmisid': '1234',
+                     'request': 'getFeature',
+                     'storedquery_id': 'fmi::observations::weather::daily::multipointcoverage'
+                     }]
+
+        expected_calls = [call(expected[0]), call(expected[1])]
+        mock_instance = mock_fmirequest.return_value
+        mock_instance.get.return_value = 'data'
+
+        handler = FMIRequestHandler('apikey')
+        result = handler.request(query, max_timespan=self._DAILY_REQUEST_MAX_RANGE_HOURS, progress_callback=None)
+
+        mock_instance.get.assert_has_calls(expected_calls)
+        assertEqual(2, mock_instance.get.call_count)
+        assertEqual(2, len(result))
