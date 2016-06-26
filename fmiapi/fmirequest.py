567fdf2766ec796aeb8844a0b0951feb3c54b31f
Tuomas Salmi <salmi.tuomas@gmail.com>
2016-06-26 11:18:27 +0300

    Logic to recover from out of range lower limit errors of the API
    Sometimes when making the request, due timezone transformations it is possible to set the beginning of the query before the lower limit of allowed range. In this case FMIApi will return special
    exception with info about allowed lower limit. This commit code will try to parse given info and retry once with given lower limit.

--- fmiapi/fmirequest.py
+++ fmiapi/fmirequest.py
@@ -1,3 +1,4 @@
+import datetime
 import http.client
 import urllib.request
 import pytz
@@ -22,7 +23,7 @@ class FMIRequest:
         self._apikey = api_key
         self._connection = http.client.HTTPConnection(self._url)
 
-    def get(self, params):
+    def _do_timezone_conversions(self, params):
         # Convert Finnish time to UTC here:
         params["starttime"] = timezone.localize(params["starttime"])
         params["endtime"] = timezone.localize(params["endtime"])
@@ -30,16 +31,33 @@ class FMIRequest:
         params["endtime"] = params["endtime"].astimezone(pytz.utc)
         params["starttime"] = params["starttime"].strftime('%Y-%m-%dT%H:%M:%SZ')
         params["endtime"] = params["endtime"].strftime('%Y-%m-%dT%H:%M:%SZ')
+        return params
+
+    def get(self, params):
+        params = self._do_timezone_conversions(params)
+        return self._do_get(params)
+
+    def _do_get(self, params, second_call=False):
         self._connection.request("GET", "/fmi-apikey/{apikey}/wfs?{query_params}"
                                  .format(apikey=self._apikey,
                                          query_params=urllib.parse.urlencode(params)),
                                  headers=self._headers)
+
         response = self._connection.getresponse()
         if response.status == 200:
             data = response.read()
             return etree.XML(data)
         else:
-            self._get_error_reason(response)
+            result = self._get_error_reason(response)
+            if result is not None and result['type'] == 'lowerlimit' and not second_call:
+                # previous request failed because of lower limit. Do new request recursively with found
+                # lower limit
+                params['starttime'] = result['lowerlimit']
+                params["starttime"] = params["starttime"].strftime('%Y-%m-%dT%H:%M:%SZ')
+                return self._do_get(params, second_call=True)
+            else:
+                # otherwise raise general RequestException
+                raise RequestException('Couldn\'t retrieve data even with found lowerlimit date {}'.format(result['lowerlimit']), response.status)
 
     def _get_error_reason(self, response):
         """
@@ -57,8 +75,47 @@ class FMIRequest:
 
         elif response.getheader("Content-Type") == "text/xml; charset=UTF8":
             xml = etree.XML(response.read())
+            if 'is out of allowed range' in etree.tostring(xml).decode('utf-8'):
+                return self._handle_out_of_range_exception(response, xml)
+
             raise RequestException(xml.find(".//xmlns:ExceptionText", namespaces=self._XMLNS_NAMESPACE).text,
                                    response.status)
+
+    def _handle_out_of_range_exception(self, response, xml):
+        """
+        FMI-api has a quirk where it will return out of range error if request's timespan begins before allowed
+        datetime. For example in Lammi Pappila station realtime data begins in 01.01.2010 02:00. If beginning time is
+        before this limit, an exception response is given which includes lowerlimit date in human readable format.
+        App tries to parse this lowerlimit datetime out from response and retry given request by replacing the beginning
+        time with found lowerlimit value.
+
+        Interestingly enough FMI api returns just empty data response for possible upperlimit cases so there is no need
+        for similar process for requests into future.
+
+        :param response:
+        :param xml:
+        :return:
+        """
+        namespaces = {'xmlns': "http://www.opengis.net/ows/1.1"}
+        elements = xml.findall('.//xmlns:ExceptionText', namespaces=namespaces)
+        try:
+            for el in elements:
+                if 'out of allowed range' in el.text:
+                    p = re.compile('lowerLimit=(.+)\)')
+                    m = p.search(el.text)
+                    if m is not None:
+                        lower_limit = m.group(1)
+                        lower_limit = datetime.datetime.strptime(lower_limit, '%Y-%b-%d %H:%M:%S')
+                        return {
+                            'type': 'lowerlimit',
+                            'lowerlimit': lower_limit
+                        }
+
+            raise RequestException(xml.find(".//xmlns:ExceptionText", namespaces=self._XMLNS_NAMESPACE).text,
+                                   response.status)
+        except:
+            raise RequestException(xml.find(".//xmlns:ExceptionText", namespaces=self._XMLNS_NAMESPACE).text,
+                               response.status)
 
     @staticmethod
     def _raise_query_limit_exception(html_str):
--- tests/fmiapi/test_fmirequest.py
+++ tests/fmiapi/test_fmirequest.py
@@ -1,5 +1,8 @@
 from fmiapi.fmirequest import FMIRequest, RequestException
+from fmiapi.fmixmlparser import FMIxmlParser
 from datetime import datetime
+
+from tests.fmiapi.testdata.expected_data import EXPECTED_REALTIME_1_DAY
 from tests.testUtils import *
 from tests.fmiapi.commonmocks import *
 from unittest import mock
@@ -52,3 +55,97 @@ def describe_fmi_request():
 
         assert_equal(404, e.value.error_code)
         assert 'Some module: Some random error' in e.value.html.decode('utf-8')
+
+    def describe_forbidden_range_error_recovery():
+        """
+        FMI-api has a quirk where it will return out of range error if request's timespan begins before allowed
+        datetime. For example in Lammi Pappila station realtime data begins in 01.01.2010 02:00. If beginning time is
+        before this limit, an exception response is given which includes lowerlimit date in human readable format.
+        App tries to parse this lowerlimit datetime out from response and retry given request by replacing the beginning
+        time with found lowerlimit value. These tests try to ensure that finding process failures are handled correctly.
+
+        Interestingly enough FMI api returns just empty data response for possible upperlimit cases so there is no need
+        for similar process for requests into future.
+        :return:
+        """
+        realtime_1_day = load_xml('./tests/fmiapi/testdata/realtime_1_day.xml')
+        out_of_allowed_range_error = load_xml('./tests/fmiapi/testdata/out_of_allowed_range_error.xml')
+        out_of_allowed_no_lowerlimit = load_xml('./tests/fmiapi/testdata/out_of_allowed_no_lowerlimit.xml')
+        out_of_allowed_invalid_lowerlimit = load_xml('./tests/fmiapi/testdata/out_of_allowed_invalid_lowerlimit.xml')
+
+        @mock.patch('http.client.HTTPConnection', spec=True)
+        def should_try_to_find_lower_limit_datetime_if_out_of_range_response_encountered_and_try_again_with_it(mock_httpconn):
+            # case where server returns out of range exception and we find instead new lower limit to retrieve with.
+            # in short, first get failure with out of range, then do a new request with success
+            mock_connection = mock_httpconn.return_value
+            mock_connection.getresponse.side_effect = [MockResponse(400, etree.tostring(out_of_allowed_range_error)),
+                                                       MockResponse(200, etree.tostring(realtime_1_day))]
+
+            fmi_request = FMIRequest('apikey')
+            query = create_daily_query(datetime(2012, 1, 9, hour=2, minute=1, second=0, microsecond=0),
+                                       datetime(2012, 1, 14, hour=2, minute=1, second=0, microsecond=0))
+            result = fmi_request.get(query)
+            # Make sure that the request was retried
+            assert_equal(2, mock_connection.getresponse.call_count)
+
+            # Returned dataframe should have all data available in provided xml
+            parser = FMIxmlParser()
+            result = parser.parse(result)
+            verify_dataframe(result, EXPECTED_REALTIME_1_DAY)
+
+        @mock.patch('http.client.HTTPConnection', spec=True)
+        def should_raise_general_request_exception_if_lowerlimit_cannot_be_found(mock_httpconn):
+            # case where error won't contain lowerlimit and regular exception should be thrown
+            mock_connection = mock_httpconn.return_value
+            mock_connection.getresponse.side_effect = [MockResponse(400, etree.tostring(out_of_allowed_no_lowerlimit))]
+
+            fmi_request = FMIRequest('apikey')
+            query = create_daily_query(datetime(2012, 1, 9, hour=2, minute=1, second=0, microsecond=0),
+                                       datetime(2012, 1, 14, hour=2, minute=1, second=0, microsecond=0))
+
+            with pytest.raises(RequestException) as e:
+                fmi_request.get(query)
+
+            # Should raise RequestException with information given by FMIApi in xml
+            assert_equal(400, e.value.error_code)
+            assert 'This is just a random error' in e.value.message
+
+        @mock.patch('http.client.HTTPConnection', spec=True)
+        def should_raise_general_request_exception_if_lowerlimit_date_cannot_be_parsed(mock_httpconn):
+            # case where error's datetime is in invalid or unsupported format
+            mock_connection = mock_httpconn.return_value
+            mock_connection.getresponse.side_effect = [MockResponse(400, etree.tostring(out_of_allowed_invalid_lowerlimit))]
+
+            fmi_request = FMIRequest('apikey')
+            query = create_daily_query(datetime(2012, 1, 9, hour=2, minute=1, second=0, microsecond=0),
+                                       datetime(2012, 1, 14, hour=2, minute=1, second=0, microsecond=0))
+
+            with pytest.raises(RequestException) as e:
+                fmi_request.get(query)
+
+            # Should raise RequestException with information given by FMIApi in xml
+            assert_equal(400, e.value.error_code)
+            assert 'value 2012-01-13 00:00:00 is out of allowed range' in e.value.message
+
+        @mock.patch('http.client.HTTPConnection', spec=True)
+        def should_not_get_into_infinite_recursion_if_lower_limit_is_invalid_and_throw_regular_regquest_exception(mock_httpconn):
+            # case where error's lowerlimit for some reason won't work and recursion should be stopped after one
+            # retry and throw Request exception with error code 400.
+            mock_connection = mock_httpconn.return_value
+            mock_connection.getresponse.side_effect = [MockResponse(400, etree.tostring(out_of_allowed_range_error)),
+                                                       MockResponse(400, etree.tostring(out_of_allowed_range_error)),
+                                                       MockResponse(400, etree.tostring(out_of_allowed_range_error))]
+
+            fmi_request = FMIRequest('apikey')
+            query = create_daily_query(datetime(2012, 1, 9, hour=2, minute=1, second=0, microsecond=0),
+                                       datetime(2012, 1, 14, hour=2, minute=1, second=0, microsecond=0))
+
+            with pytest.raises(RequestException) as e:
+                fmi_request.get(query)
+
+            # Make sure that the request was retried
+            assert_equal(2, mock_connection.getresponse.call_count)
+
+            # Should raise RequestException with information given by FMIApi in xml
+            assert_equal(400, e.value.error_code)
+            assert 'Couldn\'t retrieve data even with found lowerlimit date' in e.value.message
