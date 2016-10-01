51a65368e0a1d4a01eca5790d0cf1d5198ebe371
Tuomas Salmi <salmi.tuomas@gmail.com>
2016-10-01 15:28:06 +0300

    Refactored first catalogue fetch.
    Now on startup ui is filled only after first fetch of catalogue metadata completes. Removed some old functions.

--- gui/mainwindow.py
+++ gui/mainwindow.py
@@ -51,18 +51,17 @@ class Mainwindow(QMainWindow):
         """ Set the language of the UI """
         self._language = language
         self._app.installTranslator(self._translators[language])
-        self._set_up_supported_queries_combobox()
+        if self._current_available_datasets is not None:
+            self._set_available_datasets_from_catalogue(self._current_available_datasets)
+
 
     def changeEvent(self, event):
         if event.type() == QEvent.LanguageChange:
             self.ui.retranslateUi(self)
         super(Mainwindow, self).changeEvent(event)
 
-    def _set_up_supported_queries_combobox(self):
-        self.ui.dataSelectionCombobox.currentIndexChanged.connect(self._select_dataset_from_combobox)
-
     def _set_up_station_comboboxes(self):
-        """ Set station names for combobox in both tabs. Configure completion."""
+        """ Set station names for combobox. Configure completion."""
         self.ui.stationComboBox.clear()
         stations = self.api.get_stations()
         completer_strings = []
@@ -73,22 +72,29 @@ class Mainwindow(QMainWindow):
         self.comboboxCompleter.setCompletionMode(0)
         self.ui.stationComboBox.setCompleter(self.comboboxCompleter)
 
-        self.ui.stationComboBox.currentIndexChanged.connect(self._select_place_from_combobox)
+        # Select Lammi as a first station but do not run the selection logic on first run
         self.ui.stationComboBox.setCurrentIndex(self.api.get_index_of_station("Hämeenlinna Lammi Pappila"))
+        self._current_selected_model = self.api.get_stations()[self.api.get_index_of_station("Hämeenlinna Lammi Pappila")]
+        self.ui.stationComboBox.currentIndexChanged.connect(self._select_place_from_combobox)
 
     def _set_up_ui(self):
         # language change signal
         self.setLanguageSignal.connect(lambda: menubar_actions.select_language(self, self._settings))
 
         self._set_up_station_comboboxes()
-        self._set_up_supported_queries_combobox()
+        self.catalogue_task = BackgroundTask(self.api.get_catalogue_of_station, self._current_selected_model['FMISID'],
+                                             self._set_available_datasets_from_catalogue)
+        self.catalogue_task.start()
+
+        # When dataset is selected
+        self.ui.dataSelectionCombobox.currentIndexChanged.connect(self._select_dataset_from_combobox)
 
         # wire download buttons to actions
         self.ui.downloadButton.clicked.connect(self._download)
 
         # wire date fields to their actions
-        self.ui.endDatetimeEdit.dateChanged.connect(self._realtime_date_edited)
-        self.ui.startDatetimeEdit.dateChanged.connect(self._realtime_date_edited)
+        self.ui.endDatetimeEdit.dateChanged.connect(self._date_edited)
+        self.ui.startDatetimeEdit.dateChanged.connect(self._date_edited)
 
         # statusbar
         self.statusBar().setStyleSheet("color: red;")
@@ -105,64 +111,67 @@ class Mainwindow(QMainWindow):
     def _select_place_from_combobox(self, place_index):
         self.ui.stationComboBox.setCurrentIndex(place_index)
         self._current_selected_model = self.api.get_stations()[place_index]
-        #FIXME: REMOVE self.ui.availableFromContent.setText(self._current_selected_model["Since"])
-        self._set_time_field_limits()
 
-        # Fetch catalog information of the current station
+        # Fetch catalog information of the current station and set datasets on completion
         self.catalogue_task = BackgroundTask(self.api.get_catalogue_of_station, self._current_selected_model['FMISID'], self._set_available_datasets_from_catalogue)
         self.catalogue_task.start()
 
     def _set_available_datasets_from_catalogue(self, available_datasets):
+        """
+        Catalogue fetch completed, update contents of dataset combobox with fetched.
+        :param available_datasets:
+        :return:
+        """
         self._current_available_datasets = available_datasets
         self.ui.dataSelectionCombobox.clear()
         for q in available_datasets:
             self.ui.dataSelectionCombobox.addItem(q["name"][self._language])
-
         self.ui.dataSelectionCombobox.setCurrentIndex(0)
-
+        self._current_selected_dataset = self._current_available_datasets[0]
+        self._set_time_field_limits()
 
     @pyqtSlot(int, name='selectDataset')
     def _select_dataset_from_combobox(self, dataset_index):
-        if self._current_available_datasets is not None:
-            self._current_selected_dataset = self._current_available_datasets[dataset_index]
-            self.ui.availableFromContent.setText(datetime.datetime.strftime(self._current_selected_dataset["starttime"], '%d.%m.%Y'))
-            self._set_time_field_limits()
-
-    def get_selected_query_model(self):
-        return self.api.get_supported_queries()[self.ui.dataSelectionCombobox.currentIndex()]
+        """
+        When new dataset is selected, update time data.
+        :param dataset_index:
+        :return:
+        """
+        self._current_selected_dataset = self._current_available_datasets[dataset_index]
+        self.ui.availableFromContent.setText(datetime.datetime.strftime(self._current_selected_dataset["starttime"], '%d.%m.%Y'))
+        self._set_time_field_limits()
 
     def _set_time_field_limits(self):
-        if self._current_selected_dataset is not None:
-            # minimum date
-            self.ui.startDatetimeEdit.clearMinimumDate()
-            min_date = QDate(self._current_selected_dataset["starttime"])
-            self.ui.startDatetimeEdit.setMinimumDate(min_date)
+        # minimum date
+        self.ui.startDatetimeEdit.clearMinimumDate()
+        min_date = QDate(self._current_selected_dataset["starttime"])
+        self.ui.startDatetimeEdit.setMinimumDate(min_date)
 
-            self.ui.endDatetimeEdit.clearMinimumDate()
-            min_date = QDate(self._current_selected_dataset["starttime"])
-            self.ui.endDatetimeEdit.setMinimumDate(min_date)
+        self.ui.endDatetimeEdit.clearMinimumDate()
+        min_date = QDate(self._current_selected_dataset["starttime"])
+        self.ui.endDatetimeEdit.setMinimumDate(min_date)
 
-            # maximum date
-            self.ui.startDatetimeEdit.clearMaximumDate()
-            if self._current_selected_dataset["endtime"] is not None:
-                max_date = QDate(self._current_selected_dataset["endtime"])
-            else:
-                max_date = QDate(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
-            self.ui.startDatetimeEdit.setMaximumDate(max_date)
+        # maximum date
+        self.ui.startDatetimeEdit.clearMaximumDate()
+        if self._current_selected_dataset["endtime"] is not None:
+            max_date = QDate(self._current_selected_dataset["endtime"])
+        else:
+            max_date = QDate(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
+        self.ui.startDatetimeEdit.setMaximumDate(max_date)
 
-            self.ui.endDatetimeEdit.clearMaximumDate()
-            if self._current_selected_dataset["endtime"] is not None:
-                max_date = QDate(self._current_selected_dataset["endtime"])
-            else:
-                max_date = QDate(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
-            self.ui.endDatetimeEdit.setMaximumDate(max_date)
+        self.ui.endDatetimeEdit.clearMaximumDate()
+        if self._current_selected_dataset["endtime"] is not None:
+            max_date = QDate(self._current_selected_dataset["endtime"])
+        else:
+            max_date = QDate(datetime.datetime.now().year, datetime.datetime.now().month, datetime.datetime.now().day)
+        self.ui.endDatetimeEdit.setMaximumDate(max_date)
 
-            new_date = QDate(self.ui.startDatetimeEdit.date().year(), self.ui.startDatetimeEdit.date().month(),
-                             self.ui.startDatetimeEdit.date().day() + 1)
-            self.ui.endDatetimeEdit.setDate(new_date)
+        new_date = QDate(self.ui.startDatetimeEdit.date().year(), self.ui.startDatetimeEdit.date().month(),
+                         self.ui.startDatetimeEdit.date().day() + 1)
+        self.ui.endDatetimeEdit.setDate(new_date)
 
-            if self.ui.startDatetimeEdit.date() < self.ui.startDatetimeEdit.minimumDate():
-                self.ui.startDatetimeEdit.setDate(self.ui.startDatetimeEdit.minimumDate())
+        if self.ui.startDatetimeEdit.date() < self.ui.startDatetimeEdit.minimumDate():
+            self.ui.startDatetimeEdit.setDate(self.ui.startDatetimeEdit.minimumDate())
 
     def _choose_place_to_save_data(self, dataframe):
         paths = QStandardPaths.standardLocations(0)
@@ -182,8 +191,11 @@ class Mainwindow(QMainWindow):
             return QDateTime(dateEdit.dateTime()).toPyDateTime()
 
     @pyqtSlot(name='editRealtimeDate')
-    def _realtime_date_edited(self):
-        #realtimetab
+    def _date_edited(self):
+        """
+        Date fields were edited. Check their content for invalid date ranges.
+        :return:
+        """
         if self.ui.startDatetimeEdit.date() == self.ui.endDatetimeEdit.date():
             self.ui.startDatetimeEdit.setStyleSheet("background-color: #FC9DB7;")
             self.ui.endDatetimeEdit.setStyleSheet("background-color: #FC9DB7;")
